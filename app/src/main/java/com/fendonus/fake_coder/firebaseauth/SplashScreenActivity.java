eb51d947955cd6fd2634f640e0c4fefae53eb74a
MD. Aminul Haque Aome <aminul15-5281@diu.edu.bd>
2018-10-11 14:20:58 +0600

    Implemented Splash Screen with not showing white background while launching

--- app/src/main/java/com/fendonus/fake_coder/firebaseauth/SplashScreenActivity.java
+++ app/src/main/java/com/fendonus/fake_coder/firebaseauth/SplashScreenActivity.java
@@ -1,13 +1,26 @@
 package com.fendonus.fake_coder.firebaseauth;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private static final int SPLASH_TIME_OUT = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(mSplashTask, SPLASH_TIME_OUT);
     }
+
+    private Runnable mSplashTask = new Runnable() {
+        @Override
+        public void run() {
+            startActivity(new Intent(SplashScreenActivity.this, DashboardActivity.class));
+            finish();
+        }
+    };
 }
